77a25dde0a54b18da7c9fb3d46891d63480050b8
ju4ngutierrezramirez <ju4n.gutierrezramirez@gmail.com>
2026-06-24 22:40:02 -0600

    Primer commit

---
+++ Estrucuturas y Modulos/MiProyecto/estudiantes.py
@@ -0,0 +1,10 @@
+class Estudiante:
+    def __init__(self, nombre, edad, carrera):
+        self.nombre = nombre
+        self.edad = edad
+        self.carrera = carrera
+
+    def mostrar_informacion(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Edad: {self.edad}")
+        print(f"Carrera: {self.carrera}")
